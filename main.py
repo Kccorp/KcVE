e7fcf0e828ab5a105b1ca1360e1e424013b9b9cf
kccorp <keissy.bod@hotmail.com>
2023-01-19 17:28:21 +0100

    Clean code and add parsing file with fonctions

--- main.py
+++ main.py
@@ -1,41 +1,11 @@
-# importing the requests library
-
-
-from cveClass import *
 from request import *
+from parsing import *
 
 
 def main():
     data = callApiAll()
 
-    cveList = []
-    cpeList = []
-
-    for i in range(len(data['vulnerabilities'])):
-        cve = Cve(data['vulnerabilities'][i]['cve']['id'],
-                  data['vulnerabilities'][i]['cve']['published'],
-                  data['vulnerabilities'][i]['cve']['lastModified'],
-                  data['vulnerabilities'][i]['cve']['descriptions'][0]['value'])
-
-        cve.getCvss(data['vulnerabilities'][i]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['version'],
-                    data['vulnerabilities'][i]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore'],
-                    data['vulnerabilities'][i]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['vectorString'],
-                    data['vulnerabilities'][i]['cve']['metrics']['cvssMetricV31'][0]['exploitabilityScore'])
-
-        for j in range(len(data['vulnerabilities'][i]['cve']['configurations'][0]['nodes'][0]['cpeMatch'])):
-            cpe = data['vulnerabilities'][i]['cve']['configurations'][0]['nodes'][0]['cpeMatch'][j][
-                'criteria'].split(":")
-            cpeObject = Cpe(cpe[3], cpe[4], cpe[5])
-            cpeList.append(cpeObject)
-
-        cve.getCpe(cpeList)
-        cveList.append(cve)
-
-        cpeList = []
-
-
-
-
+    cveList = parseDataToCve(data)
 
     for i in range(len(cveList)):
         cveList[i].showAll()
--- parsing.py
+++ parsing.py
@@ -1,8 +1,37 @@
 from cpeClass import Cpe
 from cveClass import Cve
 
-def parseCpe(cpeList):
-    cpeList = cpeList['vulnerabilities'][0]['cve']['configurations'][0]['nodes'][0]['cpeMatch']
-    cpeList = cpeList[0]['cpe23Uri'].split(":")
-    cpe = Cpe(cpeList[3], cpeList[4], cpeList[5])
-    return cpe
+
+def parseAllCpeIntoList(data, cpeList, i):
+    for j in range(len(data['vulnerabilities'][i]['cve']['configurations'][0]['nodes'][0]['cpeMatch'])):
+        cpe = data['vulnerabilities'][i]['cve']['configurations'][0]['nodes'][0]['cpeMatch'][j][
+            'criteria'].split(":")
+        cpeObject = Cpe(cpe[3], cpe[4], cpe[5])
+        cpeList.append(cpeObject)
+
+    return cpeList
+
+
+def parseDataToCve(data):
+    cveList = []
+    cpeList = []
+
+    for i in range(len(data['vulnerabilities'])):
+        cve = Cve(data['vulnerabilities'][i]['cve']['id'],
+                  data['vulnerabilities'][i]['cve']['published'],
+                  data['vulnerabilities'][i]['cve']['lastModified'],
+                  data['vulnerabilities'][i]['cve']['descriptions'][0]['value'])
+
+        cve.getCvss(data['vulnerabilities'][i]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['version'],
+                    data['vulnerabilities'][i]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore'],
+                    data['vulnerabilities'][i]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['vectorString'],
+                    data['vulnerabilities'][i]['cve']['metrics']['cvssMetricV31'][0]['exploitabilityScore'])
+
+        cpeList = parseAllCpeIntoList(data, cpeList, i)
+
+        cve.getCpe(cpeList)
+        cveList.append(cve)
+
+        cpeList = []
+
+    return cveList
