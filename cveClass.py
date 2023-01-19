0ef8e0c1d0fa7ee8d711c101c43c2d05f2b96fce
kccorp <keissy.bod@hotmail.com>
2023-01-19 15:39:42 +0100

    get data and print it

--- cveClass.py
+++ cveClass.py
@@ -1,7 +1,15 @@
-
-
 # write class cve
 class Cve:
+
+    exploitabilityScore = None
+    id = None
+    publishedDate = None
+    lastModifiedDate = None
+    description = None
+    version = None
+    baseScore = None
+    vectorString = None
+
     def __init__(self, id, publishedDate, lastModifiedDate, description):
         self.id = id
         self.publishedDate = publishedDate
@@ -9,5 +17,25 @@ class Cve:
         self.description = description
 
 
+    def getCvss(self, version, baseScore, vectorString, exploitabilityScore):
+        self.version = version
+        self.baseScore = baseScore
+        self.vectorString = vectorString
+        self.exploitabilityScore = exploitabilityScore
 
+    def getCvss(self, version, baseScore, vectorString, exploitabilityScore):
+        self.version = version
+        self.baseScore = baseScore
+        self.vectorString = vectorString
+        self.exploitabilityScore = exploitabilityScore
 
+    def showAll(self):
+        print("\n")
+        print("id: " + str(self.id))
+        print("publishedDate: " + str(self.publishedDate))
+        print("lastModifiedDate: " + str(self.lastModifiedDate))
+        print("description: " + str(self.description))
+        print("version: " + str(self.version))
+        print("baseScore: " + str(self.baseScore))
+        print("vectorString: " + str(self.vectorString))
+        print("exploitabilityScore: " + str(self.exploitabilityScore))
--- main.py
+++ main.py
@@ -15,14 +15,15 @@ def main():
                   data['vulnerabilities'][i]['cve']['lastModified'],
                   data['vulnerabilities'][i]['cve']['descriptions'][0]['value'])
 
-        print(data['vulnerabilities'][i]['cve']['id'],
-                  data['vulnerabilities'][i]['cve']['published'],
-                  data['vulnerabilities'][i]['cve']['lastModified'],
-                  data['vulnerabilities'][i]['cve']['descriptions'][0]['value'])
+        cve.getCvss(data['vulnerabilities'][i]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['version'],
+                    data['vulnerabilities'][i]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['baseScore'],
+                    data['vulnerabilities'][i]['cve']['metrics']['cvssMetricV31'][0]['cvssData']['vectorString'],
+                    data['vulnerabilities'][i]['cve']['metrics']['cvssMetricV31'][0]['exploitabilityScore'])
 
         cveList.append(cve)
 
-    print("\n"+str(cveList))
+    for i in range(len(cveList)):
+        cveList[i].showAll()
 
 
 if __name__ == '__main__':
